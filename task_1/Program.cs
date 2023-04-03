9fb4fc85a321ed7dc4af44c46bdae4137049e3b2
Lyas <Egorlyasotsky@yandex.ru>
2023-04-03 10:42:09 +0400

    решена первая задача

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -2,3 +2,28 @@
 14212 -> нет
 12821 -> да
 23432 -> да */
+Console.Clear();
+Console.Write("Введите пятизначное число ");
+int n = int.Parse(Console.ReadLine());
+int[] num = new int[5];
+int N = n;
+int elem = num.Length;
+for (int i = (elem - 1); i >= 0; i--)
+{
+  N = n % 10;
+  num[i] = N;
+  n = n / 10;
+}
+if (num[0] != num[elem - 1])
+{
+  Console.Write("No");
+}
+else if (num[1] != num[elem - 2])
+{
+  Console.Write("No");
+}
+else
+{
+  Console.Write("Yes!");
+}
+
